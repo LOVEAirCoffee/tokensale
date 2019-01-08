05f146321b43d89b4f59e965099f2c1f37d0862b
Сергей Пасичник <sergey.pasichnik@nectain.com>
2019-01-08 13:12:13 +0200

    Update LAC.sol
    Added LAC smart contract

--- LAC.sol
+++ LAC.sol
@@ -1,216 +1,168 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.18;
 
-/******************************************/
-/*              SafeMath                  */
-/******************************************/
-library SafeMath {
-	function mul(uint256 a, uint256 b) internal constant returns (uint256) {
-		uint256 c = a * b;
-		assert(a == 0 || c / a == b);
-		return c;
-	}
-
-	function div(uint256 a, uint256 b) internal constant returns (uint256) {
-		uint256 c = a / b;
-		return c;
-	}
-
-	function sub(uint256 a, uint256 b) internal constant returns (uint256) {
-		assert(b <= a);
-		return a - b;
-	}
-
-	function add(uint256 a, uint256 b) internal constant returns (uint256) {
-		uint256 c = a + b;
-		assert(c >= a);
-		return c;
-	}
-}
-
-/******************************************/
-/*              ERC20 Basic               */
-/******************************************/
 contract ERC20Basic {
-  uint256 public totalSupply;
-  function balanceOf(address who) constant returns (uint256);
-  function transfer(address to, uint256 value) returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
+    uint256 public totalSupply;
+    function balanceOf(address who) public constant returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
-/******************************************/
-/*                  ERC20                */
-/******************************************/
 contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) constant returns (uint256);
-  function transferFrom(address from, address to, uint256 value) returns (bool);
-  function approve(address spender, uint256 value) returns (bool);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
+    function allowance(address owner, address spender) public constant returns (uint256);
+    function transferFrom(address from, address to, uint256 value) public returns (bool);
+    function approve(address spender, uint256 value) public returns (bool);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-/******************************************/
-/*              Basic Token               */
-/******************************************/
-contract BasicToken is ERC20Basic {
+contract LoveAirCoffee is ERC20 {
     
-  using SafeMath for uint256;
-  
-  bool enabledTransfer =false;
-  
-  modifier openedTransfer {
-        require(enabledTransfer);
-        _;
-    }
+    address owner = msg.sender;
 
-  mapping(address => uint256) balances;
-
-  function transfer(address _to, uint256 _value) openedTransfer returns (bool) {
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    Transfer(msg.sender, _to, _value);
-    return true;
-  }
-
-  function balanceOf(address _owner) constant returns (uint256 balance) {
-    return balances[_owner];
-  }
-
-}
-
-/******************************************/
-/*              Standard Token            */
-/******************************************/
-contract StandardToken is ERC20, BasicToken {
-
-  mapping (address => mapping (address => uint256)) allowed;
-
-  function transferFrom(address _from, address _to, uint256 _value) openedTransfer returns (bool) {
-    var _allowance = allowed[_from][msg.sender];
-
-    balances[_to] = balances[_to].add(_value);
-    balances[_from] = balances[_from].sub(_value);
-    allowed[_from][msg.sender] = _allowance.sub(_value);
-    Transfer(_from, _to, _value);
-    return true;
-  }
-
-  function approve(address _spender, uint256 _value) openedTransfer returns (bool) {
-
-    require((_value == 0) || (allowed[msg.sender][_spender] == 0));
-
-    allowed[msg.sender][_spender] = _value;
-    Approval(msg.sender, _spender, _value);
-    return true;
-  }
-
-  function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-    return allowed[_owner][_spender];
-  }
-
-}
-
-/******************************************/
-/*              Ownable                   */
-/******************************************/
-contract Ownable {
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowed;
     
-   address public owner;
+    // Public variables of the token
+    string public name="Love Air Coffee";
+    string public symbol="LAC";
+    uint8 public decimals = 18;
 
-    function Ownable() {
-        owner = msg.sender;
-    }
+    uint256 public totalSupply; 
+    
+    uint256 public tokensPerOneEther;
+    
+    bool public transferTokenNow=true;
+    
+    bool public frozenCoin=true;
+    
+    uint256 public minEther;
+    uint256 public maxEther;
 
-    modifier onlyOwner {
+    enum State { Disabled, Enabled }
+    
+    State public state = State.Disabled;
+
+    // This generates a public event on the blockchain that will notify clients
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    
+    // This generates a public event on the blockchain that will notify clients
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    // This notifies clients about the amount burnt
+    event Burn(address indexed from, uint256 value);
+
+    modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
     
-    function transferOwnership(address newOwner) onlyOwner {
-    require(newOwner != address(0));      
-    owner = newOwner;
-  }
-}
-
-
-/******************************************/
-/*          Crowdsale                     */
-/******************************************/
-contract Crowdsale is Ownable,StandardToken {
-    
-    using SafeMath for uint;
-    
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    
-    enum State { Disabled, Enabled }
-    
-    State public state = State.Disabled;
-    
-    uint public stageNr=0;
-    
-    uint256 public maxTokensForSale;
-
-    uint256 public rate;
-    
-    bool refundPayAfterTokenSale=false;
-    
-    mapping(uint => uint256) public stages;
-    
-    mapping(address => uint256) public balances;
-    
-    function setTransfer(bool value) onlyOwner returns(bool){
-        enabledTransfer=value;
-        return enabledTransfer;
+    function transferOwnership(address newOwner) onlyOwner public {
+        if (newOwner != address(0)) {
+            owner = newOwner;
+        }
     }
     
-    function startTokensSale(uint256 _maxTokensForSale,uint _rate,bool _refundPayAfterTokenSale) public onlyOwner {
-       require(state == State.Disabled);
-        
-        stageNr+=1;
-        maxTokensForSale=_maxTokensForSale.mul(1 ether);
-        rate=_rate;
-        refundPayAfterTokenSale=_refundPayAfterTokenSale;
+    function LoveAirCoffee(uint256 initialSupply) public{
+        totalSupply = initialSupply * 10 ** uint256(decimals);
+        balanceOf[msg.sender]=totalSupply;
+        emit Transfer(address(0),owner,totalSupply);
+    }
+    
+    function startBuyingTokens(bool _transferTokenNow,uint256 _minEther,uint256 _maxEther) public onlyOwner {
+        require(state == State.Disabled);
+        transferTokenNow = _transferTokenNow;
+        minEther = _minEther * 10 ** uint256(decimals);
+        maxEther = _maxEther * 10 ** uint256(decimals);
         state = State.Enabled;
     }
     
-    function finishSellingTokens() public onlyOwner {
+    function stopBuyingTokens() public onlyOwner {
         require(state == State.Enabled);
         state = State.Disabled;
     }
+    
+    function setFrozenCoin(bool _value) public onlyOwner {
+        frozenCoin = _value;
+    }
 
-    function() external payable {
+    // NewBuyPrice Price users can buy from the contract
+    function setPrices(uint256 newBuyPrice) onlyOwner public {
+        tokensPerOneEther = newBuyPrice;
+    }
+
+    // Buy tokens
+    function () payable external {
         require(state == State.Enabled);
-        require(msg.value > 0);
-        require(msg.sender != 0x0);
-        
-        if(refundPayAfterTokenSale){
-            uint256 tokens=msg.value.mul(rate);
-            balances[msg.sender] = balances[msg.sender].add(tokens);
-            balances[owner] = balances[owner].sub(tokens);
-            Transfer(owner, msg.sender, tokens);   
+        require(msg.value >= minEther && msg.value <= maxEther);
+        require(state == State.Enabled);
+        if(transferTokenNow){
+            uint256 tokens = (tokensPerOneEther * msg.value);
+           _transfer(owner, msg.sender, tokens);   // makes the transferss 
         }
-        
+
         owner.transfer(msg.value);
     }
-  
-  // transfer balance to owner
-	function withdrawEther() onlyOwner {
-		owner.transfer(this.balance);
-	}
-}
+    
+    function allowance(address _owner, address _spender) constant public returns (uint256) {
+        return allowed[_owner][_spender];
+    }
+    
+    function balanceOf(address _owner) constant public returns (uint256) {
+        return balanceOf[_owner];
+    }
 
-/******************************************/
-/*          LAC Token Coin                */
-/******************************************/
-contract LACTokenCoin is Crowdsale {
-    
-    string public constant name = "LOVE Air Coffee";
-    
-    string public constant symbol = "LAC";
-    
-    uint32 public constant decimals = 18;
-    
-    function LACTokenCoin(uint256 _totalSupply){
-        totalSupply=_totalSupply.mul(1 ether);
-        balances[msg.sender]=totalSupply;
-        Transfer(address(0x0), msg.sender, totalSupply);
+    /* Internal transfer, only can be called by this contract */
+    function _transfer(address _from, address _to, uint256 _value) internal {
+        require (_to != address(0x0));                          // Prevent transfer to 0x0 address. Use burn() instead
+        require (balanceOf[_from] >= _value);                   // Check if the sender has enough
+        require (balanceOf[_to] + _value >= balanceOf[_to]);    // Check for overflows
+        balanceOf[_from] -= _value;                             // Subtract from the sender
+        balanceOf[_to] += _value;                               // Add the same to the recipient
+        emit Transfer(_from, _to, _value);
+    }
+
+    // Transfer tokens
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+         require(!frozenCoin);
+        _transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    // Transfer tokens from other address
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(!frozenCoin);
+        require(_value <= allowed[_from][msg.sender]);     // Check allowance
+        allowed[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+
+    //Allows `_spender` to spend no more than `_value` tokens in your behalf
+    function approve(address _spender, uint256 _value) public
+        returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    //Destroy tokens
+    function burn(uint256 _value) public returns (bool success) {
+        require(!frozenCoin);
+        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
+        balanceOf[msg.sender] -= _value;            // Subtract from the sender
+        totalSupply -= _value;                      // Updates totalSupply
+        emit Burn(msg.sender, _value);
+        return true;
+    }
+
+    //Destroy tokens from other account
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        require(!frozenCoin);
+        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
+        require(_value <= allowed[_from][msg.sender]);    // Check allowance
+        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
+        allowed[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
+        totalSupply -= _value;                              // Update totalSupply
+        emit Burn(_from, _value);
+        return true;
     }
 }
